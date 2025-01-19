5a474c4fd5a3c8f6ba67d0c17633c4aec0e963d2
Longbo Hou <1140480869@qq.com>
2025-01-19 13:42:14 +0800

    Commit message

--- ILQR代码/demo.m
+++ ILQR代码/demo.m
@@ -18,6 +18,8 @@ Xlog = zeros(arg.num_states,arg.tf/arg.dt);
 U = zeros(arg.num_ctrl);
 
 for i=1:arg.tf/arg.dt
+    hold on;
+    axis equal;
     fprintf('---------- 仿真第%d步 ------------\n ',i);
     [Xnew,Unew] = ilqr(X);
     % i
@@ -25,16 +27,17 @@ for i=1:arg.tf/arg.dt
     % Unew(1,2)
     X = updateState(Xnew(1,:),Unew(1,:));
     Xlog(1:2,i) = X(1:2);
-    scatter(arg.xcoord,arg.ycoord);
-    hold on;
-    scatter(Xlog(1,1:i),Xlog(2,1:i),1);
+    % scatter(arg.xcoord,arg.ycoord);
+    scatter(arg.xcoord, arg.ycoord, 30, [0.678, 0.847, 0.902], 'filled');
+    scatter(Xlog(1,1:i),Xlog(2,1:i),3,'red');
     fprintf('------------速度V=%f\n------------前轮转角=%f\n',X(4),rad2deg(Unew(1,2)));
-    scatter(Xnew(:,1),Xnew(:,2),0.5,"green");
+    scatter(Xnew(:,1),Xnew(:,2),3,"green");
     % scatter(arg.Xnominal(:,1),arg.Xnominal(:,2),0.5,"green")
-    scatter(X(1),X(2),5,"yellow");
-    hold off;
+    scatter(X(1),X(2),10,"yellow");
+    rectangle('Position', [300, 0, 5, 2], 'EdgeColor', 'r', 'FaceColor', 'r', 'LineWidth', 1);
+    
     drawnow;
-
+    hold off;
 end
 
 
